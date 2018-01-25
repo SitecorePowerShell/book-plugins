ffde3f7c381bdf37cea85782cc9ecb9281a569b7
Unknown <michaellwest@gmail.com>
2018-01-25 12:14:10 -0600

    Added check for invalid date.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 module.exports = {
     hooks: {
         "page:before": function (page) {
+            var time = "{{ file.mtime }}"
+            if (time === "Invalid Date") {
+                return page;
+            }
             var content = page.content;
             content = ">*Last modified: {{ file.mtime }}*\n\n" + content; 
             page.content = content; 
